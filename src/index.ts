6a5a08710caaa60d413b471e35294a16c34e9c2e
Rhys van der Waerden <rhys.vdw@gmail.com>
2021-11-30 20:26:03 +1100

    Add sidebar generator

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,29 @@
-console.log("Hello!");
+import fs from "fs";
+import path from "path";
+
+const root = process.argv.length > 2 ? process.argv[2] : process.cwd();
+const tab = "  ";
+
+function addDirectoryItems(doc: string, dirPath: string, indent: string) {
+  const files = fs.readdirSync(dirPath);
+
+  return files.reduce((acc, filename) => {
+    if (!(filename.startsWith(".") || filename.startsWith("_"))) {
+      const absPath = path.join(dirPath, filename);
+      const stats = fs.lstatSync(absPath);
+      if (stats.isDirectory()) {
+        acc += `${indent}- ${filename}\n`
+        acc = addDirectoryItems(acc, absPath, indent + tab);
+      } else if (filename.endsWith(".md") && stats.isFile()) {
+        const name = filename.slice(0, filename.length - ".md".length);
+        acc += `${indent}- [[${name}|${name}]]\n`
+      }
+    }
+    return acc;
+  }, doc);
+}
+
+const result = addDirectoryItems("", root, "");
+const outPath = path.join(root, "_Sidebar.md");
+fs.writeFileSync(outPath, result);
+console.log(`Created ${outPath}`);
