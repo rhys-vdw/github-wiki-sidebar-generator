c9ab66d90cf06253f446e602fae39ca8fc5e8fa7
Rhys van der Waerden <rhys.vdw@gmail.com>
2021-12-01 14:57:45 +1100

    Support .wikiignore

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
 # github-wiki-sidebar-generator
 
-Run in root of wiki project to generate bullet sidebar with nesting based on folder structure.
+Run in root of wiki project to generate bullet sidebar with nesting based on folder structure. Creates `_Sidebar.md` which is expcted by GitHub wiki.
 
 Run `github-wiki-sidebar-generator -h` for help.
+
+# .wikiignore
+
+Include an ignore file in your project root to exclude some directories from the generated sidebar:
+
+```ignore
+# .wikiignore
+
+/node_modules/
+```
--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,45 @@
 import fs from "fs";
 import path from "path";
+const ignore = require("ignore-file") as any;
 
 const tab = "  ";
 
-function addDirectoryItems(doc: string, dirPath: string, indent: string) {
+function addDirectoryItems(
+  doc: string,
+  rootPath: string,
+  dirPath: string,
+  indent: string,
+  filter: (path: string) => boolean
+): string {
   const files = fs.readdirSync(dirPath);
 
   return files.reduce((acc, filename) => {
     if (!(filename.startsWith(".") || filename.startsWith("_"))) {
       const absPath = path.join(dirPath, filename);
-      const stats = fs.lstatSync(absPath);
-      if (stats.isDirectory()) {
-        acc += `${indent}- ${filename}\n`
-        acc = addDirectoryItems(acc, absPath, indent + tab);
-      } else if (filename.endsWith(".md") && stats.isFile()) {
-        const name = filename.slice(0, filename.length - ".md".length);
-        acc += `${indent}- [[${name}|${name}]]\n`
+      const relPath = path.relative(rootPath, absPath);
+      if (!filter(relPath)) {
+        const stats = fs.lstatSync(absPath);
+        if (stats.isDirectory()) {
+          acc += `${indent}- ${filename}\n`;
+          acc = addDirectoryItems(acc, rootPath, absPath, indent + tab, filter);
+        } else if (filename.endsWith(".md") && stats.isFile()) {
+          const name = filename.slice(0, filename.length - ".md".length);
+          acc += `${indent}- [[${name}|${name}]]\n`;
+        }
+      } else {
+        console.log("skipping " + absPath);
       }
     }
     return acc;
   }, doc);
 }
 
-export function generate(root: string) {
-  return addDirectoryItems("", root, "");
+export function generate(root: string): string {
+  const filter = ignore.sync(".wikiignore") || ignore.compile("");
+  return addDirectoryItems("", root, root, "", filter);
 }
 
-export function write(root: string) {
+export function write(root: string): void {
   const outPath = path.join(root, "_Sidebar.md");
   fs.writeFileSync(outPath, generate(root));
   console.log(`Created ${outPath}`);
